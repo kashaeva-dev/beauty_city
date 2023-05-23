ade103748f866b4fa0ae6dc96eca79945e304569
Amartyanov1974 <randomquake@yandex.ru>
2023-05-23 21:13:02 +0400

    зарегистрировал классы

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Client
+from .models import Procedure
+from .models import Salon
+from .models import Master
+from .models import Schedule
+from .models import Record
+
+admin.site.register(Client)
+admin.site.register(Procedure)
+admin.site.register(Salon)
+admin.site.register(Master)
+admin.site.register(Schedule)
+admin.site.register(Record)
 # Register your models here.
